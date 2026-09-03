a3f9c5ebfd8516068f8ccf7eb8af9f77789bb063
Pawapon Thammalangka <108130529+ParkPawapon@users.noreply.github.com>
2026-09-04 01:07:37 +0700

    feat(hijri): implement Umm al-Qura calendar adapter interface and barrel exports

---
+++ src/calendar/hijri/index.ts
@@ -0,0 +1,18 @@
+export {
+  islamicCivilAdapter,
+  ISLAMIC_CIVIL_IDENTITY,
+  islamicCivilValidator,
+  isIslamicCivilLeapYear,
+  daysInIslamicCivilMonth,
+} from "./civil-adapter.js";
+
+export {
+  islamicTblaAdapter,
+  ISLAMIC_TBLA_IDENTITY,
+  islamicTblaValidator,
+} from "./tabular-adapter.js";
+
+export {
+  islamicUmalquraAdapter,
+  ISLAMIC_UMALQURA_IDENTITY,
+} from "./umm-al-qura-adapter.js";
